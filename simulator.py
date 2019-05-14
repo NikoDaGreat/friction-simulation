96db43bd4b92b8c096d903ac027b764c6b39fba5
Niko Savola <niko.savola@aalto.fi>
2019-05-14 15:20:51 +0300

    z cells

--- simulator.py
+++ simulator.py
@@ -18,10 +18,10 @@ def equi_dist(xy):
 xy_cells_Al = xy_cells(4.046)
 equi_dist_Al = equi_dist(xy_cells(4.046))
 # create the lattices used
-sim.create_slab(element='Al',xy_cells=xy_cells(4.046),z_cells=3,bottom_z=3.0)
+sim.create_slab(element='Al',xy_cells=xy_cells(4.046),z_cells=2,bottom_z=3.0)
 # sim.create_slab(element='Hg',xy_cells=1,z_cells=1,top_z=1.0)
 sim.create_random_atoms(50, 'Hg', 0.5, 2.5, minimum_distance=1.2)
-sim.create_slab(element='Al',xy_cells=xy_cells(4.046),z_cells=3,top_z=0.0)
+sim.create_slab(element='Al',xy_cells=xy_cells(4.046),z_cells=2,top_z=0.0)
 
 
 sim.list_atoms() # print atoms to terminal for debug purposes
@@ -31,11 +31,11 @@ sim.create_interaction(['Al','Al'], strength=1.0, equilibrium_distance=equi_dist
 sim.create_interaction(['Hg','Hg'], strength=0.7, equilibrium_distance=1.39)
 sim.create_interaction(['Al','Hg'], strength=0.2, equilibrium_distance=4.39)
 
-Al_top_indices = sim.get_indices_z_more_than(4.5)
+Al_top_indices = sim.get_indices_z_more_than(2.9)
 Al_bot_indices= sim.get_indices_z_less_than(-2.5)
 Hg_indices = sim.get_indices_by_element('Hg')
 
-sim.set_velocities(indices=Al_top_indices, velocity=[0, 0.05, 0])
+sim.fix_velocities(indices=Al_top_indices, velocity=[0, 0.05, 0])
 
 
 sim.fix_positions(Al_bot_indices)
