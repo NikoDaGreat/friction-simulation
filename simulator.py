10ea72102668c58c891053940495b0c845e0b9de
Niko Savola <niko.savola@aalto.fi>
2019-05-14 16:11:32 +0300

    speedy boi

--- simulator.py
+++ simulator.py
@@ -18,28 +18,30 @@ def equi_dist(xy):
 xy_cells_Al = xy_cells(4.046)
 equi_dist_Al = equi_dist(xy_cells(4.046))
 # create the lattices used
-sim.create_slab(element='Al',xy_cells=xy_cells(4.046),z_cells=2,bottom_z=10.0)
+sim.create_slab(element='Al',xy_cells=xy_cells(4.046),z_cells=3,bottom_z=12.0)
 # sim.create_slab(element='Hg',xy_cells=1,z_cells=1,top_z=1.0)
-#sim.create_random_atoms(50, 'Hg', 0.5, 2.5, minimum_distance=1.2)
-sim.create_slab(element='Al',xy_cells=xy_cells(4.046),z_cells=2,top_z=0.0)
+sim.create_random_atoms(50, 'Hg', 2.0, 10, minimum_distance=-0.4+equi_dist(xy_cells(3)))
+sim.create_slab(element='Al',xy_cells=xy_cells(4.046),z_cells=3,top_z=0.0)
 
 
 sim.list_atoms() # print atoms to terminal for debug purposes
 
 #the potential energy well (in eV) and the atomic separation (in Å)
 sim.create_interaction(['Al','Al'], strength=1.0, equilibrium_distance=equi_dist_Al)
-#sim.create_interaction(['Hg','Hg'], strength=0.7, equilibrium_distance=1.39)
-#sim.create_interaction(['Al','Hg'], strength=0.2, equilibrium_distance=4.39)
+sim.create_interaction(['Hg','Hg'], strength=0.15, equilibrium_distance=-0.4+equi_dist(xy_cells(3)))
+sim.create_interaction(['Al','Hg'], strength=0.32, equilibrium_distance=2)
 
-Al_top_indices = sim.get_indices_z_more_than(2.9)
-Al_bot_indices= sim.get_indices_z_less_than(-2.5)
-#Hg_indices = sim.get_indices_by_element('Hg')
+Al_top_indices = sim.get_indices_z_more_than(12.0)
+Al_bot_indices= sim.get_indices_z_less_than(-3.5)
+Hg_indices = sim.get_indices_by_element('Hg')
 
-#sim.fix_velocities(indices=Al_top_indices, velocity=[0, 0.05, 0])
+sim.fix_velocities(indices=Al_top_indices, velocity=[0, 0.005, 0])
 
-
+# pohjaslabin pohja pysyy paikallaan
 sim.fix_positions(Al_bot_indices)
 
+# yläslabille painovoima
+sim.add_constant_force(sim.get_indices_z_more_than(15.0),[0,0,-0.05])
 
 # default settings
 sim.create_dynamics(dt=1.0)
@@ -48,7 +50,7 @@ sim.create_dynamics(dt=1.0)
 sim.save_trajectory_during_simulation(interval=global_interval, filename='{}.traj'.format(filename)) # 5 fs
 sim.gather_energy_and_temperature_during_simulation(interval=global_interval, filename='energy.txt')
 sim.gather_average_position_during_simulation(interval=global_interval,indices=Al_top_indices,filename='Al_position.txt')
-#sim.gather_average_position_during_simulation(interval=global_interval,indices=Hg_indices,filename='Hg_position.txt')
+sim.gather_average_position_during_simulation(interval=global_interval,indices=Hg_indices,filename='Hg_position.txt')
 
 # - monitor the simulation by printing info to stdout
 sim.print_stats_during_simulation(interval=50)
