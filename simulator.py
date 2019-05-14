8422cb8b917380c77c5a72ced55f0e7316d9e21d
Niko Savola <niko.savola@aalto.fi>
2019-05-14 12:36:55 +0300

    removed tutorial

--- simulator.py
+++ simulator.py
@@ -8,21 +8,22 @@ global_interval = 10 # femtoseconds
 sim = ft.FrictionSimulation()
 
 # create the lattices used
-sim.create_slab(element='Au',xy_cells=5,z_cells=2,bottom_z=2.0)
-sim.create_slab(element='Zn',xy_cells=5,z_cells=2,bottom_z=-1.0)
+sim.create_slab(element='Al',xy_cells=4,z_cells=2,top_z=2.0)
+sim.create_slab(element='Al',xy_cells=4,z_cells=2,top_z=-1.0)
+
 
 sim.list_atoms() # print atoms to terminal for debug purposes
 
 # needed interactions
-sim.create_interaction(['Au','Au'], strength=1.0, equilibrium_distance=2.39)
-sim.create_interaction(['Zn','Zn'], strength=0.7, equilibrium_distance=1.39)
-sim.create_interaction(['Au','Zn'], strength=0.2, equilibrium_distance=4.39)
+sim.create_interaction(['Al','Al'], strength=1.0, equilibrium_distance=2.39)
+sim.create_interaction(['Hg','Hg'], strength=0.7, equilibrium_distance=1.39)
+sim.create_interaction(['Al','Hg'], strength=0.2, equilibrium_distance=4.39)
 
-au_indices = sim.get_indices_by_element('Au')
-Zn_indices = sim.get_indices_by_element('Zn')
+Al_indices = sim.get_indices_by_element('Au')
+Hg_indices = sim.get_indices_by_element('Zn')
 
 
-sim.set_velocities(indices=au_indices, velocity=[0, 0.5, 0])
+sim.set_velocities(indices=Al_indices, velocity=[0, 0.5, 0])
 
 # default settings
 sim.create_dynamics(dt=1.0)
@@ -30,8 +31,8 @@ sim.create_dynamics(dt=1.0)
 
 sim.save_trajectory_during_simulation(interval=global_interval, filename='{}.traj'.format(filename)) # 5 fs
 sim.gather_energy_and_temperature_during_simulation(interval=global_interval, filename='energy.txt')
-sim.gather_average_position_during_simulation(interval=global_interval,indices=au_indices,filename='au_position.txt')
-sim.gather_average_position_during_simulation(interval=global_interval,indices=Zn_indices,filename='Zn_position.txt')
+sim.gather_average_position_during_simulation(interval=global_interval,indices=Al_indices,filename='Al_position.txt')
+sim.gather_average_position_during_simulation(interval=global_interval,indices=Hg_indices,filename='Hg_position.txt')
 
 # - monitor the simulation by printing info to stdout
 sim.print_stats_during_simulation(interval=50)
