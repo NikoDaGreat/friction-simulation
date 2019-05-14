bfafd16fafb47aa5aba2b259c929a9e2f795e073
Niko Savola <niko.savola@aalto.fi>
2019-05-14 14:27:06 +0300

    kaikki nyt Al

--- simulator.py
+++ simulator.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 # coding=utf-8
 import friction_tools as ft
+import numpy as np
 import time
 
 # init variables
@@ -8,22 +9,28 @@ filename = 'simulation'
 global_interval = 10 # femtoseconds
 sim = ft.FrictionSimulation()
 
+def xy_cells(lattice_constant):
+    return np.round(10.0/lattice_constant)
+
+def equi_dist(xy):
+    return np.divide(np.sqrt(50), xy)
+
+xy_cells_Al = xy_cells(4.046)
+equi_dist_Al = equi_dist(xy_cells(4.046))
 # create the lattices used
-sim.create_slab(element='Al',xy_cells=4,z_cells=3,top_z=5.0)
+sim.create_slab(element='Al',xy_cells=xy_cells(4.046),z_cells=3,top_z=5.0)
 sim.create_slab(element='Hg',xy_cells=1,z_cells=1,top_z=1.0)
-sim.create_slab(element='Ag',xy_cells=4,z_cells=3,top_z=0.0) # oikeasti myös Al
+sim.create_slab(element='Al',xy_cells=xy_cells(4.046),z_cells=3,top_z=0.0)
 
 
 sim.list_atoms() # print atoms to terminal for debug purposes
 
 #the potential energy well (in eV) and the atomic separation (in Å)
-sim.create_interaction(['Al','Al'], strength=1.0, equilibrium_distance=2.39)
-sim.create_interaction(['Ag','Ag'], strength=1.0, equilibrium_distance=2.39)
+sim.create_interaction(['Al','Al'], strength=1.0, equilibrium_distance=equi_dist_Al)
 sim.create_interaction(['Hg','Hg'], strength=0.7, equilibrium_distance=1.39)
 sim.create_interaction(['Al','Hg'], strength=0.2, equilibrium_distance=4.39)
-sim.create_interaction(['Ag','Hg'], strength=0.2, equilibrium_distance=4.39)
 
-Al_top_indices = sim.get_indices_by_element('Al')
+Al_top_indices = sim.get_indices_z_more_than(4.5)
 Al_bot_indices= sim.get_indices_z_less_than(-2.5)
 Hg_indices = sim.get_indices_by_element('Hg')
 
@@ -39,7 +46,7 @@ sim.create_dynamics(dt=1.0, temperature=300)
 
 sim.save_trajectory_during_simulation(interval=global_interval, filename='{}.traj'.format(filename)) # 5 fs
 sim.gather_energy_and_temperature_during_simulation(interval=global_interval, filename='energy.txt')
-sim.gather_average_position_during_simulation(interval=global_interval,indices=Al_indices,filename='Al_position.txt')
+sim.gather_average_position_during_simulation(interval=global_interval,indices=Al_top_indices,filename='Al_position.txt')
 sim.gather_average_position_during_simulation(interval=global_interval,indices=Hg_indices,filename='Hg_position.txt')
 
 # - monitor the simulation by printing info to stdout
