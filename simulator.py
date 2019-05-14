98b9f337fc8cfdc4ed99aca369d2c0d8c39f9ae2
Niko Savola <niko.savola@aalto.fi>
2019-05-14 15:24:03 +0300

    rem hg

--- simulator.py
+++ simulator.py
@@ -20,7 +20,7 @@ equi_dist_Al = equi_dist(xy_cells(4.046))
 # create the lattices used
 sim.create_slab(element='Al',xy_cells=xy_cells(4.046),z_cells=2,bottom_z=3.0)
 # sim.create_slab(element='Hg',xy_cells=1,z_cells=1,top_z=1.0)
-sim.create_random_atoms(50, 'Hg', 0.5, 2.5, minimum_distance=1.2)
+#sim.create_random_atoms(50, 'Hg', 0.5, 2.5, minimum_distance=1.2)
 sim.create_slab(element='Al',xy_cells=xy_cells(4.046),z_cells=2,top_z=0.0)
 
 
@@ -28,12 +28,12 @@ sim.list_atoms() # print atoms to terminal for debug purposes
 
 #the potential energy well (in eV) and the atomic separation (in Å)
 sim.create_interaction(['Al','Al'], strength=1.0, equilibrium_distance=equi_dist_Al)
-sim.create_interaction(['Hg','Hg'], strength=0.7, equilibrium_distance=1.39)
-sim.create_interaction(['Al','Hg'], strength=0.2, equilibrium_distance=4.39)
+#sim.create_interaction(['Hg','Hg'], strength=0.7, equilibrium_distance=1.39)
+#sim.create_interaction(['Al','Hg'], strength=0.2, equilibrium_distance=4.39)
 
 Al_top_indices = sim.get_indices_z_more_than(2.9)
 Al_bot_indices= sim.get_indices_z_less_than(-2.5)
-Hg_indices = sim.get_indices_by_element('Hg')
+#Hg_indices = sim.get_indices_by_element('Hg')
 
 sim.fix_velocities(indices=Al_top_indices, velocity=[0, 0.05, 0])
 
@@ -48,7 +48,7 @@ sim.create_dynamics(dt=1.0, temperature=300)
 sim.save_trajectory_during_simulation(interval=global_interval, filename='{}.traj'.format(filename)) # 5 fs
 sim.gather_energy_and_temperature_during_simulation(interval=global_interval, filename='energy.txt')
 sim.gather_average_position_during_simulation(interval=global_interval,indices=Al_top_indices,filename='Al_position.txt')
-sim.gather_average_position_during_simulation(interval=global_interval,indices=Hg_indices,filename='Hg_position.txt')
+#sim.gather_average_position_during_simulation(interval=global_interval,indices=Hg_indices,filename='Hg_position.txt')
 
 # - monitor the simulation by printing info to stdout
 sim.print_stats_during_simulation(interval=50)
