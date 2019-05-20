738f75291e5bbaecd58168813c89b25fbf06a824
Niko Savola <niko.savola@aalto.fi>
2019-05-20 15:57:55 +0300

    toimivahko kitkakerroin 0.05

--- plotGraphs.m
+++ plotGraphs.m
@@ -8,7 +8,10 @@ y=data(:,2);
 z=data(:,3).*(-1);
 
 %xy=sqrt( x.^2 + y.^2 ); % pythagoraan lause
+z_force=0.05;
+y_force=mean(y);
 
+mu=-y_force/z_force
 
 lm = fitlm(z,y,'linear');
 tspace = linspace(0,1,100)';
@@ -18,10 +21,10 @@ tspace = linspace(0,1,100)';
 plot(lm)
 % ,'x--','Color',[86.3, 0, 42.4]./100,'LineWidth',1.6
 
-xlabel('Load $N$')
-ylabel('Friction force $F$')
-title('Kisulin namujen m{\"a}{\"a}r{\"a} ajan funktiona')
-legend('Trendik{\"a}yr{\"a}','Lineaarinen sovite','$99\%$-luottamusv{\"a}lit','location','SouthEast')
+xlabel('Load $N$','interpreter','latex')
+ylabel('Friction force $F$','interpreter','latex')
+title('Friction with liquid lubrication ($Hg$)','interpreter','latex')
+%legend('','','','location','SouthEast')
 grid on
 
 print('kitka','-dpng')
