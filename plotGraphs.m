ae530e4b2c59ada6e14ad28db0e94df459121a22
Elias-YJ <elias.yla-jarkko@live.fi>
2019-05-21 18:12:15 +0300

    Plotteja Matlabiin

--- plotGraphs.m
+++ plotGraphs.m
@@ -1,13 +1,28 @@
 clear all
 close all
-forces = linspace(0,0.1,11);
+
+%Editoi tätä listaa kun haluat skannata eri nimisiä tiedostoja.
+forces = [0.00 0.01]
 for currentForce = forces
     txt = sprintf('%.2f', currentForce);
     data = importdata(strcat(strcat('Al_forces_', txt),'.txt'),' ',0);
+   
+    
     %x=data(:,1);
     y=data(:,2);
     z=data(:,3).*(-1);
-
+    time=25.*(1:length(y));
+    
+    figure
+    subplot(2,1,1)
+    plot(time,y,'r.');hold on;plot(time,z,'k*');grid on;xlabel('Aika (fs)');ylabel('Voima (eV/Å)')
+    legend('Y','Z');
+    
+    subplot(2,1,2)
+    plot(time,y,'r-');grid on;xlabel('Aika (fs)');ylabel('Voima (eV/Å)')
+    legend('Y')
+    
+    
     %xy=sqrt( x.^2 + y.^2 ); % pythagoraan lause
     z_force=mean(z);
     y_force=mean(y);
