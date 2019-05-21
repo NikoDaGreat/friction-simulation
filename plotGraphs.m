d5e1c1794d849a4b25dd466e726777ab4f81d01f
Niko Savola <niko.savola@aalto.fi>
2019-05-21 16:37:19 +0300

    For-loopattu kitkakerroinkeräin

--- plotGraphs.m
+++ plotGraphs.m
@@ -1,30 +1,32 @@
 clear all
 close all
+forces = linspace(0,0.5,11)
+for currentForce = forces
+    txt = sprintf('%.1f', currentForce);
+    data = importdata(strcat(strcat('Al_forces_', txt),'.txt'),' ',0);
+    %x=data(:,1);
+    y=data(:,2);
+    z=data(:,3).*(-1);
 
+    %xy=sqrt( x.^2 + y.^2 ); % pythagoraan lause
+    z_force=mean(z);
+    y_force=mean(y);
 
-data = importdata('Al_forces.txt',' ',0);
-%x=data(:,1);
-y=data(:,2);
-z=data(:,3).*(-1);
+    mu=-y_force/z_force
+    % 
+    % lm = fitlm(z,y,'linear');
+    % tspace = linspace(0,1,100)';
+    % [ypred,yci] = predict(lm,tspace,'Alpha',0.01);
+    % plot(lm)
+    % % ,'x--','Color',[86.3, 0, 42.4]./100,'LineWidth',1.6
+    % 
+    % xlabel('Load $N$','interpreter','latex')
+    % ylabel('Friction force $F$','interpreter','latex')
+    % title('Friction with liquid lubrication ($Hg$)','interpreter','latex')
+    % %legend('','','','location','SouthEast')
+    % grid on
 
-%xy=sqrt( x.^2 + y.^2 ); % pythagoraan lause
-z_force=0.05;
-y_force=mean(y);
-
-mu=-y_force/z_force
-
-lm = fitlm(z,y,'linear');
-tspace = linspace(0,1,100)';
-[ypred,yci] = predict(lm,tspace,'Alpha',0.01);
-
-
-plot(lm)
-% ,'x--','Color',[86.3, 0, 42.4]./100,'LineWidth',1.6
-
-xlabel('Load $N$','interpreter','latex')
-ylabel('Friction force $F$','interpreter','latex')
-title('Friction with liquid lubrication ($Hg$)','interpreter','latex')
-%legend('','','','location','SouthEast')
-grid on
-
-print('kitka','-dpng')
+    %print('kitka','-dpng')
+    %writematrix(,mu)
+    save(strcat(strcat('mu_', txt),'.txt'), 'mu', '-ASCII','-append');
+end
--- simulator.py
+++ simulator.py
@@ -3,6 +3,8 @@
 import friction_tools as ft
 import numpy as np
 import time
+#tiedostojen lukemiseen
+
 
 # init variables
 filename = 'simulation'
@@ -17,53 +19,77 @@ def equi_dist(xy):
 
 xy_cells_Al = xy_cells(4.046)
 equi_dist_Al = equi_dist(xy_cells(4.046))
-# create the lattices used
-sim.create_slab(element='Al',xy_cells=xy_cells(4.046),z_cells=3,bottom_z=12.0)
-sim.create_slab(element='Hg',xy_cells=1, z_cells=1, top_z=7.0)
-#sim.create_random_atoms(50, 'Hg', 2.0, 10, minimum_distance=-0.4+equi_dist(xy_cells(3)))
-sim.create_slab(element='Al',xy_cells=xy_cells(4.046),z_cells=3,top_z=0.0)
 
 
-sim.list_atoms() # print atoms to terminal for debug purposes
-
-#the potential energy well (in eV) and the atomic separation (in Å)
-sim.create_interaction(['Al','Al'], strength=1.0, equilibrium_distance=equi_dist_Al)
-sim.create_interaction(['Hg','Hg'], strength=0.30, equilibrium_distance=equi_dist(1) )
-sim.create_interaction(['Al','Hg'], strength=0.6, equilibrium_distance=2+1)
-
-Al_top_indices = sim.get_indices_z_more_than(12.0)
-Al_toppest_indices = sim.get_indices_z_more_than(12.0+6)
-Al_bot_indices= sim.get_indices_z_less_than(-3.5)
-Hg_indices = sim.get_indices_by_element('Hg')
-
-sim.fix_velocities(indices=Al_toppest_indices, velocity=[0, 0.005, 0], xyz=[True,True,False])
-
-# pohjaslabin pohja pysyy paikallaan
-sim.fix_positions(Al_bot_indices)
-
-# yläslabille painovoima
-sim.add_constant_force(sim.get_indices_z_more_than(15.0),[0,0,-0.05])
-
-# default settings
-sim.set_temperature(temperature=273) # huoneenlämpö
-sim.create_dynamics(dt=global_interval, temperature=273, coupled_indices=Al_bot_indices)
+#simuloidut voimat(testi)
+forces = np.linspace(0,1,11)
 
 
-sim.save_trajectory_during_simulation(interval=global_interval, filename='{}.traj'.format(filename)) # 5 fs
-sim.gather_energy_and_temperature_during_simulation(interval=global_interval, filename='energy.txt')
-sim.gather_average_position_during_simulation(interval=global_interval,indices=Al_top_indices,filename='Al_position.txt')
-sim.gather_average_position_during_simulation(interval=global_interval,indices=Hg_indices,filename='Hg_position.txt')
-sim.gather_average_force_during_simulation(interval=global_interval,indices=Al_toppest_indices,filename='Al_forces.txt')
 
-# - monitor the simulation by printing info to stdout
-sim.print_stats_during_simulation(interval=50)
+for N in forces:
+    #keskimääräinen y-suuntainen voima
+    MeanYs = np.zeros(np.size(forces))
 
-t0 = time.time()
-# run the simulation for 1000 fs
-sim.run_simulation(time=10000.0)
-t1 = time.time()
+    #keskimääräinen z-suuntainen voima
+    MeanZs = np.zeros(np.size(forces))
 
-print "time taken {ti} s".format(ti=str(int(t1-t0)))
+    # create the lattices used
+    sim.create_slab(element='Al',xy_cells=xy_cells(4.046),z_cells=3,bottom_z=12.0)
+    sim.create_slab(element='Hg',xy_cells=1, z_cells=1, top_z=7.0)
+    #sim.create_random_atoms(50, 'Hg', 2.0, 10, minimum_distance=-0.4+equi_dist(xy_cells(3)))
+    sim.create_slab(element='Al',xy_cells=xy_cells(4.046),z_cells=3,top_z=0.0)
+
+
+    sim.list_atoms() # print atoms to terminal for debug purposes
+
+    #the potential energy well (in eV) and the atomic separation (in Å)
+    sim.create_interaction(['Al','Al'], strength=1.0, equilibrium_distance=equi_dist_Al)
+    sim.create_interaction(['Hg','Hg'], strength=0.30, equilibrium_distance=equi_dist(1) )
+    sim.create_interaction(['Al','Hg'], strength=0.6, equilibrium_distance=2+1)
+
+    Al_top_indices = sim.get_indices_z_more_than(12.0)
+    Al_toppest_indices = sim.get_indices_z_more_than(12.0+6)
+    Al_bot_indices= sim.get_indices_z_less_than(-3.5)
+    Hg_indices = sim.get_indices_by_element('Hg')
+
+    sim.fix_velocities(indices=Al_top_indices, velocity=[0, 0.005, 0], xyz=[True,True,False])
+
+    # pohjaslabin pohja pysyy paikallaan
+    sim.fix_positions(Al_bot_indices)
+
+    # yläslabille painovoima
+    sim.add_constant_force(sim.get_indices_z_more_than(15.0),[0,0,-0.05])
+
+    # default settings
+    sim.set_temperature(temperature=273) # huoneenlämpö
+    sim.create_dynamics(dt=global_interval, temperature=273, coupled_indices=Al_bot_indices)
+
+
+    sim.save_trajectory_during_simulation(interval=global_interval, filename='{}_{}.traj'.format(filename, N)) # 5 fs
+    #sim.gather_energy_and_temperature_during_simulation(interval=global_interval, filename='energy.txt')
+    #sim.gather_average_position_during_simulation(interval=global_interval,indices=Al_top_indices,filename='Al_position.txt')
+    #sim.gather_average_position_during_simulation(interval=global_interval,indices=Hg_indices,filename='Hg_position.txt')
+    sim.gather_average_force_during_simulation(interval=global_interval,indices=Al_toppest_indices,filename='Al_forces_{}.txt'.format(N))
+
+    # - monitor the simulation by printing info to stdout
+    sim.print_stats_during_simulation(interval=50)
+
+    t0 = time.time()
+    # run the simulation for 1000 fs
+    sim.run_simulation(time=10000.0)
+    t1 = time.time()
+
+    print "time taken {ti} s".format(ti=str(int(t1-t0)))
+    #avataan tekstitiedosto
+    file=np.loadtxt('Al_forces_{}.txt'.format(N))
+    #tallennetaan voimat vektoreihin
+    #MeanYs[length(MeanYs)] = np.mean(file[int(np.round(length(MeanYs)/2)):,1])
+    #MeanZs[length(MeanZs)] = np.mean(file[int(np.round(length(MeanZs)/2)):,2])
+
+    #coeff = [np.mean(file[(np.size(file)/2):][:,1]), np.mean(file[(np.size(file)/2):][:,2]) ]
+    #np.save("coefficient_{}".format(N), coeff )
+    ft.trajectory_to_xyz(filename_in='{}_{}.traj'.format(filename, N), filename_out='{}_{}.xyz'.format(filename, N))
+
+
 
 # generated .traj to VMD-supported .xyz
-ft.trajectory_to_xyz(filename_in='{}.traj'.format(filename), filename_out='{}.xyz'.format(filename))
