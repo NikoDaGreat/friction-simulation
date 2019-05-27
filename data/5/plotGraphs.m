8c5e36a491036379ad2604576470a60c400c0ffd
Elias-YJ <elias.yla-jarkko@live.fi>
2019-05-27 16:36:15 +0300

    Update files

--- data/5/plotGraphs.m
+++ data/5/plotGraphs.m
@@ -3,7 +3,7 @@ clear all
 close all
 
 % Editoi t???t??? listaa kun haluat skannata eri nimisi??? tiedostoja.
-forces = [0, 0.0100, 0.0200, 0.0300, 0.0400, 0.0500, 0.0600, 0.0700, 0.0800, 0.0900, 0.1000];
+forces = [0.0100, 0.0200, 0.0300, 0.0400, 0.0500, 0.0600];
 %forces = [0.10 0.15 0.20];
 for currentForce = forces
     txt = sprintf('%.2f', currentForce);
